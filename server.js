b5e58b8e97f630f4373600f2bcc3d0bed808509f
asafgb <asafshazar@gmail.com>
2018-05-24 08:28:03 +0300

    Get photo + base 64 + delete

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.post('/api/Check',jsonParser, async function (req, res){//,expressKerberos()
             var list =await getMoviesFromApiAsync();
             var choosen=list[req.body.itemId-1];
             //var x=getBase64(choosen.path)
-            var x = load(choosen.path);
+            var x = load(choosen.path ,res);
             //.then(data=>)
-            res.send({error:false,message:'Your picture Is upload'});
+//            res.send({error:false,message:'Your picture Is upload'});
 //            var x =2;
         }
         else
@@ -77,52 +77,27 @@ app.post('/api/Check',jsonParser, async function (req, res){//,expressKerberos()
 */
 
 app.listen(port, ()=> console.log(`start on ${port}`))
-// async function saveIt(picurl)
-// {
-//     requests.get(picurl)
-//     // .on('end',()=>{
-//     //     var m="sd";
-//     // })
-//     .on('close',()=>{
-//         var m="sd";
-//     })
-//     .on('response', function(response) {
-//         console.log(response.statusCode) // 200
-//         console.log(response.headers['content-type']) // 'image/png'
-//       })
-//       .pipe(fs.createWriteStream('doodle.png'))
-      
-// }
-function load(picurl)
+function load(picurl ,res)
 {
     //להוסיף מספר random
-    //requests(picurl).pipe(fs.createWriteStream('doodle.png'))
-//    await saveIt(picurl)
-requests.get(picurl)
-    .on('close',()=>{
-        fs.readFile('doodle.png', (err, data)=>{
-            let base64Image = new Buffer(data, 'binary').toString('base64');
-            fs.unlink('doodle.png', function(error) {
-                if (error) {
-                    throw error;
-                }
-                console.log('Deleted doodle.png');
-            });
-        })
-    })
-      .pipe(fs.createWriteStream('doodle.png'))
+  requests.get(picurl)
+  .on('close',()=>{
+      fs.readFile('doodle.png', (err, data)=>{
+          let base64Image = new Buffer(data, 'binary').toString('base64');
+          res.send({error:false,message:'Your picture Is upload'});
+          fs.unlink('doodle.png', function(error) {
+              if (error) {
+                  throw error;
+              }
+              console.log('Deleted doodle.png');
+          });
+      })
+  })
+    .pipe(fs.createWriteStream('doodle.png'))
 
-    
+  
 }
 
-/*function getBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = error => reject(error);
-    });
-  }*/
 
  function getMoviesFromApiAsync() {
     return fetch('https://api.myjson.com/bins/t97a2')//')//'"https://feeds.citibikenyc.com/stations/stations.json"')
